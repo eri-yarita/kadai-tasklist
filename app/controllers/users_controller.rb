b2919fe3d59f118f8345f93964f696335ce17d7f
eri yarita <jyf4_wxcd63@yahoo.co.jp>
2020-10-15 11:39:23 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,12 @@ class UsersController < ApplicationController
     end
   end
   
+  def show
+    @user = User.find(params[:id])
+    @tasks = @user.tasks.order(id: :desc).page(params[:page])
+    counts(@user)
+  end
+  
   private
 
   def user_params
